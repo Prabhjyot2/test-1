df6b38a59f739e479f53a20002764c0942c4b016
KHULAJATT <49119867+Prabhjyot2@users.noreply.github.com>
2021-04-29 15:48:25 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect(" remotemysql.com", "fqsmB7PAk8", "esccsYs90f", "remote sql");
+$con=mysqli_connect(" remotemysql.com", "fqsmB7PAk8", "esccsYs90f", "fqsmB7PAk8");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
